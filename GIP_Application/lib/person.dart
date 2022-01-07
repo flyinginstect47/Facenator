1e5afba426919a78731e3499886da38319fb0701
VhArno <95482605+VhArno@users.noreply.github.com>
2022-01-07 13:22:36 +0100

    Create person.dart

---
+++ GIP_Application/lib/person.dart
@@ -0,0 +1,11 @@
+class Person {
+  String VoorNaam;
+  String Naam;
+  String Klas;
+  
+  Person(String VoorNaam, String Naam, String Klas) {
+    this.VoorNaam = VoorNaam;
+    this.Naam = Naam;
+    this.Klas = Klas;
+  }
+}
