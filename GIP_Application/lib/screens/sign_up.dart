b080d4dadfaca2257882228d196a0ded95314f84
VhArno <95482605+VhArno@users.noreply.github.com>
2022-04-12 19:55:48 +0200

    Create sign_up.dart

---
+++ GIP_Application/lib/screens/sign_up.dart
@@ -0,0 +1,497 @@
+import 'dart:async';
+// ignore: unused_import
+import 'dart:isolate';
+import 'dart:ui';
+// ignore: unused_import
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// ignore: unused_import
+import 'package:gip_application/screens/menu.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+//import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+String username = '';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPage();
+}
+
+class _SignUpPage extends State<SignUpPage> with TickerProviderStateMixin {
+  late AnimationController controller1;
+  late AnimationController controller2;
+  late Animation<double> animation1;
+  late Animation<double> animation2;
+  late Animation<double> animation3;
+  late Animation<double> animation4;
+
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController email = TextEditingController();
+
+  // ignore: non_constant_identifier_names
+  Future SignUp(BuildContext cont) async {
+    if (username.text == "" || password.text == "") {
+      Fluttertoast.showToast(
+        msg: "Beide velden moeten ingevuld zijn!",
+        toastLenght: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        fontSize: 16.0,
+      );
+    } else {
+      var url = "http://192.168.56.1/localconnect/InsertUser.php";
+      var response = await http.post(url, body: {
+        "username": username.text,
+        "password": password.text,
+        "email": email.text,
+      });
+
+      var data = await json.decode(response.body);
+
+      if (data == "success") {
+        Navigator.pushNamed(context, "/login");
+      } else {
+        Fluttertoast.showToast(
+          msg: "De gebruikersnaam en wachtwoord combinatie klopt niet!",
+          toastLenght: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          fontSize: 16.0,
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller1 = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 5,
+      ),
+    );
+    animation1 = Tween<double>(begin: .1, end: .15).animate(
+      CurvedAnimation(
+        parent: controller1,
+        curve: Curves.easeInOut,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller1.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller1.forward();
+        }
+      });
+    animation2 = Tween<double>(begin: .02, end: .04).animate(
+      CurvedAnimation(
+        parent: controller1,
+        curve: Curves.easeInOut,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    controller2 = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 5,
+      ),
+    );
+    animation3 = Tween<double>(begin: .41, end: .38).animate(CurvedAnimation(
+      parent: controller2,
+      curve: Curves.easeInOut,
+    ))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller2.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller2.forward();
+        }
+      });
+    animation4 = Tween<double>(begin: 170, end: 190).animate(
+      CurvedAnimation(
+        parent: controller2,
+        curve: Curves.easeInOut,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    Timer(const Duration(milliseconds: 2500), () {
+      controller1.forward();
+    });
+
+    controller2.forward();
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    controller2.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: const Color(0xff192028),
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: size.height * (animation2.value + .58),
+                  left: size.width * .21,
+                  child: CustomPaint(
+                    painter: MyPainter(50),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .98,
+                  left: size.width * .1,
+                  child: CustomPaint(
+                    painter: MyPainter(animation4.value - 30),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .5,
+                  left: size.width * (animation2.value + .8),
+                  child: CustomPaint(
+                    painter: MyPainter(30),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * animation3.value,
+                  left: size.width * (animation1.value + .1),
+                  child: CustomPaint(
+                    painter: MyPainter(60),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .1,
+                  left: size.width * .8,
+                  child: CustomPaint(
+                    painter: MyPainter(animation4.value),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Padding(
+                        padding: EdgeInsets.only(top: size.height * .1),
+                        child: Text(
+                          'Facenator',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(.7),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            wordSpacing: 4,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Padding(
+                        padding: EdgeInsets.only(top: size.height * .1),
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(
+                            color: Colors.grey.withOpacity(.7),
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            wordSpacing: 4,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 12,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          component1(Icons.account_circle_outlined,
+                              'User name...', false, false),
+                          component4(
+                              Icons.email_outlined, 'E-mail...', false, true),
+                          component2(
+                              Icons.lock_outline, 'Password...', true, false),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              component3(
+                                'Create account',
+                                2.58,
+                                () {
+                                  HapticFeedback.lightImpact();
+                                  SignUp(context);
+                                },
+                              ),
+                              SizedBox(width: size.width / 20),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          component3(
+                            'Log In',
+                            2,
+                            () {
+                              HapticFeedback.lightImpact();
+                              Navigator.pushNamed(context, "/");
+                              Fluttertoast.showToast(
+                                  msg: 'Create a new account button pressed');
+                            },
+                          ),
+                          SizedBox(height: size.height * .05),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component1(
+      IconData icon, String hintText, bool isPassword, bool isEmail) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaY: 15,
+          sigmaX: 15,
+        ),
+        child: Container(
+          height: size.width / 8,
+          width: size.width / 1.2,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(right: size.width / 30),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.05),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: TextField(
+            controller: username,
+            style: TextStyle(color: Colors.white.withOpacity(.8)),
+            cursorColor: Colors.white,
+            obscureText: isPassword,
+            keyboardType:
+                isEmail ? TextInputType.emailAddress : TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.white.withOpacity(.7),
+              ),
+              border: InputBorder.none,
+              hintMaxLines: 1,
+              hintText: hintText,
+              hintStyle:
+                  TextStyle(fontSize: 14, color: Colors.white.withOpacity(.5)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component4(
+      IconData icon, String hintText, bool isPassword, bool isEmail) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaY: 15,
+          sigmaX: 15,
+        ),
+        child: Container(
+          height: size.width / 8,
+          width: size.width / 1.2,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(right: size.width / 30),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.05),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: TextField(
+            controller: email,
+            style: TextStyle(color: Colors.white.withOpacity(.8)),
+            cursorColor: Colors.white,
+            obscureText: isPassword,
+            keyboardType:
+                isEmail ? TextInputType.emailAddress : TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.white.withOpacity(.7),
+              ),
+              border: InputBorder.none,
+              hintMaxLines: 1,
+              hintText: hintText,
+              hintStyle:
+                  TextStyle(fontSize: 14, color: Colors.white.withOpacity(.5)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component2(
+      IconData icon, String hintText, bool isPassword, bool isEmail) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaY: 15,
+          sigmaX: 15,
+        ),
+        child: Container(
+          height: size.width / 8,
+          width: size.width / 1.2,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(right: size.width / 30),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.05),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: TextField(
+            controller: password,
+            style: TextStyle(color: Colors.white.withOpacity(.8)),
+            cursorColor: Colors.white,
+            obscureText: isPassword,
+            keyboardType:
+                isEmail ? TextInputType.emailAddress : TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.white.withOpacity(.7),
+              ),
+              border: InputBorder.none,
+              hintMaxLines: 1,
+              hintText: hintText,
+              hintStyle:
+                  TextStyle(fontSize: 14, color: Colors.white.withOpacity(.5)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component3(String string, double width, VoidCallback voidCallback) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaY: 15, sigmaX: 15),
+        child: InkWell(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onTap: voidCallback,
+          child: Container(
+            height: size.width / 8,
+            width: size.width / width,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(.05),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text(
+              string,
+              style: TextStyle(color: Colors.white.withOpacity(.8)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+        .add(DiagnosticsProperty<Animation<double>>('animation3', animation3));
+  }
+}
+
+class Fluttertoast {
+  static void showToast(
+      {String? msg, Toast? toastLenght, double? fontSize, gravity}) {}
+}
+
+class MyPainter extends CustomPainter {
+  final double radius;
+
+  MyPainter(this.radius);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..shader = const LinearGradient(
+              colors: [Color(0xffFD5E3D), Color(0xffC43990)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)
+          .createShader(Rect.fromCircle(
+        center: const Offset(0, 0),
+        radius: radius,
+      ));
+
+    canvas.drawCircle(Offset.zero, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  // ignore: todo
+  // TODO: implement build
+  throw UnimplementedError();
+}
