9a2d228fd0c59e6abea02af4b57316f7247dd45c
VhArno <95482605+VhArno@users.noreply.github.com>
2022-05-18 23:45:09 +0200

    Update main.dart

--- GIP_Application/lib/main.dart
+++ GIP_Application/lib/main.dart
@@ -1,12 +1,24 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:gip_application/screens/Edit_PF-Page.dart';
+import 'package:gip_application/screens/Profile_Page.dart';
 import 'package:gip_application/screens/login_page.dart';
 import 'package:gip_application/screens/menu.dart';
+import 'package:gip_application/screens/sign_up.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(const App());
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   var email = prefs.getString('email');
+//   print(email);
+//   runApp(MaterialApp(home: email == null ? LogInPage() : HomePage()));
+// }
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -28,7 +40,56 @@ class _AppState extends State<App> {
         '/login': (context) => const LogInPage(),
         '/menu': (context) => const Menu(),
         '/SignIn': (context) => const SignUpPage(),
+        '/EditPage': (context) => const EditPage(),
+        '/ProfilePage': (context) => const ProfilePage(),
       },
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/clinician_splash.png"),
+              fit: BoxFit.cover),
+        ),
+      ),
+    );
+  }
+
+  void startTimer() {
+    Timer(Duration(seconds: 3), () {
+      navigateUser(); //It will redirect  after 3 seconds
+    });
+  }
+
+  void navigateUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var status = prefs.getBool('isLoggedIn') ?? false;
+    print(status);
+    if (status) {
+      Navigator.pushNamed(context, "/menu");
+    } else {
+      Navigator.pushNamed(context, "/Login");
+    }
   }
 }
